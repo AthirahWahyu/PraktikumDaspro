fe9afd26cb295aa2e5468d7bb4e4b89a13c6ab6e
AthirahWahyu <athirahsasongko07@gmail.com>
2025-10-10 11:12:46 +0700

    Percobaan 1 : Penerapan IF dan IF-ELSE untuk Mencetak KRS Pertanyaan 1 : Mengapa pengecekan pada struktur IF tersebut tidak melibatkan kondisi dengan operator relasional? Jawaban : Pengecekan pada struktur IF tersebut tidak melibatkan operator relasional karena Variabel yang dicek (uktLunas) sudah bertipe boolean, sehingga langsung bisa digunakan sebagai kondisi tanpa perlu dibandingkan lagi.

---
+++ jobsheet5/ifCetakKRS4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ifCetakKRS4{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        //kode berikut ini untuk menerima masukan dari keyboard yang kemudian
+        //disimpan pada variabel uktLunas bertipe boolean
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false) : ");
+        boolean uktLunas = sc.nextBoolean();
+
+        //struktur pemilihan IF untuk mengecek apakah UKT sudah lunas
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        }
+        sc.close();
+    }
+}
